fb5373b27f05eab85ed405299138e607b3795318
Jeffry Hesse <jhesse@sonatype.com>
2020-02-13 16:15:55 -0900

    Ok yargs, now there's an executable index.js

--- src/index.ts
+++ src/index.ts
@@ -14,19 +14,66 @@
  * limitations under the License.
  */
 import { CycloneDXSbomCreator } from "./CycloneDXSbomCreator/CycloneDXSbomCreator";
+import yargs, { Argv } from 'yargs';
 
-let sbomCreator = new CycloneDXSbomCreator(
-  process.cwd(), {
-    devDependencies: true, 
-    includeBomSerialNumber: true, 
-    includeLicenseData: true,
-    includeLicenseText: true,
-  });
+let argv = yargs
+  .help()
+  .scriptName('cyclonedx-bom')
+  .command(
+    'generate [options]',
+    'Generate a CycloneDX SBOM',
+    (y: Argv) => {
+      return y.options({
+        'include-serial-number': {
+          alias: "ns",
+          type: "boolean",
+          demandOption: false,
+          description: "Do not generate bom serial number"
+        },
+        dev: {
+          alias: "d",
+          type: "boolean",
+          description: "Include Development Dependencies",
+          demandOption: false
+        },
+        'exclude-license-data': {
+          alias: "nl",
+          type: "boolean",
+          description: "Exclude License Data",
+          demandOption: false
+        },
+        'exclude-license-text': {
+          alias: "nlt",
+          type: "boolean",
+          description: "Exclude License Text",
+          demandOption: false
+        }
+      })
+    }
+  )
+  .argv;
 
-sbomCreator.createBom()
-  .then((val) => {
-    console.log(val);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+
+if (argv) {
+  if (argv._[0] == 'generate') {
+    let sbomCreator = new CycloneDXSbomCreator(
+      process.cwd(), {
+        devDependencies: (argv.dev) ? true : false, 
+        includeBomSerialNumber: (argv["include-serial-number"]) ? true : false, 
+        includeLicenseData: (argv["exclude-license-data"]) ? false : true,
+        includeLicenseText: (argv["exclude-license-text"]) ? false : true,
+      });
+    
+    sbomCreator.createBom()
+      .then((val) => {
+        console.log(val);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  } else {
+    yargs.showHelp();
+    process.exit(0);
+  }
+}
+
